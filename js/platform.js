79a93217ad97eac3c812b204e26b10da2b741917
Charles McTophat <nazimmerman29@gmail.com>
2015-06-17 14:29:35 -0500

    Update platform.js

--- js/platform.js
+++ js/platform.js
@@ -21,11 +21,11 @@ var map = {
       [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
       [2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
       [2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-      [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+      [2, 9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
       [2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
       [2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2],
       [2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2],
-      [2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 2, 9, 2, 1, 1, 1, 1, 1, 1, 2],
       [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     ],
     
@@ -71,6 +71,140 @@ var Standard = function() {
     y: 200
   };
   
-  this.camera =
-}
+  this.camera = {
+    x: 0,
+    y: 0
+  };
+  
+  this.key = {
+    left: false,
+    right: false,
+    up: false
+  };
+  
+  this.player = {
     
+    loc: {
+      x: 0,
+      y: 0
+    },
+    
+    vel: {
+      x: 0,
+      y: 0
+    },
+    
+    can_jump: true
+  };
+  
+  window.onkeydown = this.keydown.bind(this);
+  window.onkeyup = this.keyup.bind(this);
+};
+
+Standard.prototype.error = function(message) {
+  if(this.alert_errors) alert(message);
+  if(this.log_info) console.log(message);
+};
+
+Standard.prototype.log = function(message) {
+  if(this.log_info) console.log(message);
+};
+
+Standard.prototype.set_viewpoint = function(x, y) {
+  this.viewport.x = x;
+  this.viewport.y = y;
+};
+
+Standard.prototype.keydown = function(e) {
+  var _this = this;
+  
+  switch(e.keyCode) {
+    case 65:
+      _this.key.left = true;
+      break;
+    case 83:
+      _this.key.up = true;
+      break;
+    case 68: 
+      _this.key.right = true;
+      break;
+  }
+};
+
+Standard.prototype.keyup = function(e) {
+  var _this = this;
+  
+  switch(e.keyCode) {
+    case 65:
+      _this.key.left = false;
+      break;
+    case 83:
+      _this.key.up = false;
+      break;
+    case 68: 
+      _this.key.right = false;
+      break;
+  }
+};
+
+Standard.prototype.load_map = function(map) {
+  if(typeof map === 'undefined'
+  || typeof map.data === 'undefined'
+  || typeof map.keys === 'undefined') {
+    this.error('Error: invalid map data');
+    return false;
+  }
+  
+  this.current_map = map;
+  this.current_map.background = map.background || '#333';
+  this.current_map.gravity = map.gravity || {x: 0, y: 0.3};
+  this.tile_size = map.tile_size || 16;
+  
+  var _this = this;
+  
+  this.current_map.width = 0;
+  this.current_map.height = 0;
+  
+  map.keys.forEach(function(key) {
+    map.data.forEach(function (row, y) {
+      _this.current_map.height = Math.max(_this.current_map.height, y);
+      
+      row.forEach(function(tile, x) {
+        _this.current_map.width = Math.max(_this.current_map.width, x);
+        
+        if(tile == key.id)
+          _this.current_map.data[y][x] = key;
+      });
+    });
+  });
+  
+  this.current_map.width_p = this.current_map.width * this.tile_size;
+  this.current_map.height_p = this.current_map.height * this.tile_size;
+  this.player.loc.x = map.player.x * this.tile_size || 0;
+  this.player.loc.y = map.player.y * this.tile_size || 0;
+  this.player.colour = map.player.colour || '#000';
+  
+  this.key.left  = false;
+  this.key.up    = false;
+  this.key.right = false;
+    
+  this.camera = {
+      x: 0,
+      y: 0
+  };
+    
+  this.player.vel = {
+      x: 0,
+      y: 0
+  };
+
+  this.log('Successfully loaded map data.');
+
+  return true;
+};
+
+Standard.prototype.get_tile = function(x, y) {
+  return(this.current_map.data[y] && this.current_map.data[y][x])?
+  this.current_map.data[y][x] : 0;
+};
+
